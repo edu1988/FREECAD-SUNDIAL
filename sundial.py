d5fe8b3c3567fccb88d21a563e42e210f39e4d40
Eduu <edu@edudaw.com>
2022-11-29 18:05:07 +0100

    Posicionamiento numeros romanos

--- sundial.py
+++ sundial.py
@@ -3,7 +3,7 @@ import Draft
 import math
 
 # Parámetros principales
-LATITUD             = 42
+LATITUD             = 41.98
 MINUTE_GAP          = 5
 TOTAL_HOURS         = 7.75
 
@@ -18,6 +18,9 @@ TENTH_LINES_LENGTH  = 6                       #longitud de las líneas de los di
 FIVE_MIN_PTS_GAP    = 3                       #distancia del borde a los puntos de 5 minutos
 FIVE_MIN_PTS_RAD    = 1                       #radio de los puntos de 5 minutos
 LINES_WITDH         = 0.5                     #grosor líneas horarias
+HOURS_NUMBERS_GAP   = 20                      #distancia de los números de las horas a las lineas de las horas
+FONT_PATH           = "D:/Mis Documentos/FUENTES/goldoni/Goldoni_DEMO.otf"
+FONT_SIZE           = 16.0
 
 # Parámetros calculados
 posXgnomon          = (GNOMON_WIDTH / 2) * -1
@@ -26,26 +29,23 @@ llineashoras        = RADIO - BORDER_GAP - HOURS_LINES_LENGTH
 llineasmedias       = RADIO - BORDER_GAP - HALF_LINES_LENGTH
 llineasdieces       = RADIO - BORDER_GAP - TENTH_LINES_LENGTH
 posXlineaModelo     = (GNOMON_WIDTH / 2) - (LINES_WITDH / 2)
+distanciaHorasBorde = HOURS_LINES_LENGTH + HOURS_NUMBERS_GAP + BORDER_GAP
 
-configHoras = {
-  'horas': HOURS_LINES_LENGTH,
-  'medias': HALF_LINES_LENGTH,
-}
 
 def gradosToRadianes(grados):
-  return grados * 2 * math.pi / 360
+  return grados * math.pi / 180
 
 def radianesToGrados(radianes):
-   return radianes / (2 * math.pi / 360)
+   return radianes * 180 / math.pi
 
-def pos(angulo):
-  f = 1 if angulo > 0 else -1
+def pos(angulo, margen):
+  f = 1 if angulo > 0 and angulo <= 90 else -1
   k = math.cos(gradosToRadianes(angulo))
   h = math.sin(gradosToRadianes(angulo))
   c = radianesToGrados(math.atan(GNOMON_WIDTH / (2 * GNOMON_X_POS))) * f
   l = math.sqrt(pow(GNOMON_WIDTH/2, 2) + pow(GNOMON_X_POS, 2))
   g = math.cos(gradosToRadianes(90 - angulo + c))
-  r = RADIO - FIVE_MIN_PTS_GAP
+  r = RADIO - margen
   i = pow(l, 2) * pow(g, 2) - pow(l, 2) + pow(r, 2)
   s = math.sqrt(i)
   u = (GNOMON_WIDTH / 2) * f
@@ -108,6 +108,7 @@ offst = Draft.offset(FreeCAD.ActiveDocument.Circle, RADIO - BORDER_GAP, copy=Tru
 offst = Draft.offset(FreeCAD.ActiveDocument.Circle, RADIO - FIVE_MIN_PTS_GAP, copy=True, occ=False)
 FreeCAD.getDocument('SUNDIAL').getObject('Circle002').MakeFace = False
 FreeCAD.getDocument(nameDocument).getObject('Circle002').Label = 'CirculoCincoMinutos'
+Gui.activeDocument().Circle002.Visibility=False
 
 # Circulo Horas (Circle003)
 offst = Draft.offset(FreeCAD.ActiveDocument.Circle001, llineashoras, copy=True, occ=False)
@@ -152,8 +153,8 @@ TOTAL_LINEAS = int(TOTAL_HOURS * (60 / MINUTE_GAP) + 1)
 Draft.make_polar_array(App.ActiveDocument.Rectangle002, number=TOTAL_LINEAS, angle=-90.0, center=FreeCAD.Vector(posXgnomon * -1, posYgnomon, 0.0), use_link=True)
 
 
-# Draft.autogroup(_obj_)
 FreeCAD.getDocument(nameDocument).getObject('Array').ExpandArray = True
+Gui.activeDocument().Array.Visibility=False
 FreeCAD.ActiveDocument.recompute()
 
 # Dibujar linea modelo 5 minutos
@@ -166,6 +167,7 @@ line = Draft.make_wire(points, placement=pl, closed=False, face=True, support=No
 # # Dibujar todas las lineas
 Draft.make_polar_array(App.ActiveDocument.Line, number=TOTAL_LINEAS, angle=-90.0, center=FreeCAD.Vector(posXgnomon * -1, posYgnomon, 0.0), use_link=True)
 FreeCAD.getDocument(nameDocument).getObject('Array001').ExpandArray = True
+Gui.activeDocument().Array001.Visibility=False
 FreeCAD.ActiveDocument.recompute()
 
 nameElemsRect = 'Array_i'
@@ -174,6 +176,21 @@ nameElemsLine = 'Array001_i'
 SALTO = 360 / (24 * (60 / MINUTE_GAP))
 SIN = math.sin(gradosToRadianes(LATITUD))
 XDEV = LINES_WITDH / 2
+
+# Array de circulos cinco minutos para fusionarlos posteriormente en un solo objeto
+arrayCirculos = []
+arrayHoras    = []
+
+horas = {
+  '0':  ['XII', 'XII'],
+  '12': ['I', 'XI'],
+  '24': ['II', 'X'],
+  '36': ['III', 'IX'],
+  '48': ['IV', 'VIII'],
+  '60': ['V', 'VI'],
+  '72': ['VI', 'VI'],
+  '84': ['VII', 'V']
+}
 
 for x in range(TOTAL_LINEAS):
     grosor = GNOMON_WIDTH
@@ -206,7 +223,7 @@ for x in range(TOTAL_LINEAS):
 
     if(x % 2 != 0):
 
-      coords = pos(anguloFinalPuntos)
+      coords = pos(anguloFinalPuntos, FIVE_MIN_PTS_GAP)
       posXcircle = coords['x']
       posYcircle = coords['y']
 
@@ -215,10 +232,65 @@ for x in range(TOTAL_LINEAS):
       pl.Base=FreeCAD.Vector(posXcircle, posYcircle, 0.0)
       circle = Draft.make_circle(radius=0.6, placement=pl, face=True, support=None)
 
+      # Añadir circulo a un array para fusionarlos despues en un solo objeto
+      arrayCirculos.append(circle)
+
+    if(x % 12 == 0):
+      
+      coordsNumsD = pos(anguloFinalPuntos, distanciaHorasBorde)
+      coordsNumsI = pos(180 - anguloFinalPuntos, distanciaHorasBorde)
+      posXnumD = coordsNumsD['x']
+      posYnumD = coordsNumsD['y']
+      posXnumI = coordsNumsI['x']
+      posYnumI = coordsNumsI['y']
+
+      key = str(x)
+      hora1 = horas[key][0]
+      hora2 = horas[key][1]
+
+      ss = Draft.make_shapestring(String=hora1, FontFile=FONT_PATH, Size=FONT_SIZE, Tracking=0.0)
+      
+      moveX = (ss.Shape.BoundBox.XLength / 2)
+      moveY = (ss.Shape.BoundBox.YLength / 2)
+      posXnumD -= moveX
+      posYnumD -= moveY
+
+      if(x == 0):
+        posXnumD -= (GNOMON_WIDTH / 2)
+      
+      plm=FreeCAD.Placement()
+      plm.Base=FreeCAD.Vector(posXnumD, posYnumD, 0.0)
+      plm.Rotation.Q=(0.0, 0.0, 0.0, 1.0)
+      ss.Placement=plm
+      ss.Support=None
+
+      arrayHoras.append(ss)
+
+      if(x != 0):
+        ss = Draft.make_shapestring(String=hora2, FontFile=FONT_PATH, Size=FONT_SIZE, Tracking=0.0)
+        
+        moveX = (ss.Shape.BoundBox.XLength / 2)
+        moveY = (ss.Shape.BoundBox.YLength / 2)
+        posXnumI -= moveX
+        posYnumI -= moveY
+        
+        plm=FreeCAD.Placement()
+        plm.Base=FreeCAD.Vector(posXnumI, posYnumI, 0.0)
+        plm.Rotation.Q=(0.0, 0.0, 0.0, 1.0)
+        ss.Placement=plm
+        ss.Support=None
+
+        arrayHoras.append(ss)
+      
+
     FreeCAD.getDocument(nameDocument).getObject(nameArrayRect).Placement = App.Placement(App.Vector(a,b,0.00),App.Rotation(App.Vector(0.00,0.00,1.00),anguloFinal))
     FreeCAD.getDocument(nameDocument).getObject(nameArrayLine).Placement = App.Placement(App.Vector(j,k,0.00),App.Rotation(App.Vector(0.00,0.00,1.00),anguloFinal))
 
+App.activeDocument().addObject("Part::MultiFuse","FusionCirculosCinco")
+App.activeDocument().FusionCirculosCinco.Shapes = arrayCirculos
 
+App.activeDocument().addObject("Part::MultiFuse","FusionNumerosHoras")
+App.activeDocument().FusionNumerosHoras.Shapes = arrayHoras
 
 # Marca mediodia
 App.activeDocument().addObject("Part::MultiCommon","MarcaMediodia")
@@ -227,10 +299,16 @@ Gui.activeDocument().Rectangle001.Visibility=False
 Gui.activeDocument().AroHoras.Visibility=False
 
 # Marcas horas
+marcasHoras = range(1, TOTAL_LINEAS)
+
+
 nameAroHoras  = 'AroHoras'
 nameAroMedias = 'AroMedias'
 nameAroDieces = 'AroDieces'
-for x in range(TOTAL_LINEAS):
+
+arrayMarcas = []
+
+for x in marcasHoras:
   nameObjectR = 'Array_i' + str(x)
   nameObjectF = 'LINEAHORA' + str(x)
   aro = lineKind(x, 12)
@@ -248,11 +326,21 @@ for x in range(TOTAL_LINEAS):
   App.activeDocument().addObject("Part::MultiCommon",nameObjectF)
   objectF = App.getDocument(nameDocument).getObject(nameObjectF)
   objectF.Shapes = [object1,object2]
+
+  arrayMarcas.append(objectF)
+
   objectF.recompute()
 
+  
 
+App.activeDocument().addObject("Part::MultiFuse","FusionLineas")
+App.activeDocument().FusionLineas.Shapes = arrayMarcas
 
+App.activeDocument().addObject("Part::MultiFuse","MarcasHorarias")
+App.activeDocument().MarcasHorarias.Shapes = [App.activeDocument().FusionCirculosCinco,App.activeDocument().FusionLineas]
+Gui.activeDocument().FusionCirculosCinco.Visibility=False
+Gui.activeDocument().FusionLineas.Visibility=False
 
-
+m = Draft.mirror([FreeCAD.ActiveDocument.MarcasHorarias], FreeCAD.Vector(0.0, -1.0, 0.0), FreeCAD.Vector(0.0, 1.0, 0.0))
 
 FreeCAD.ActiveDocument.recompute()
